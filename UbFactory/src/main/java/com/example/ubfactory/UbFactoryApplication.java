8df37d3ac0669b08757da9d2e253fbf37ccd0ba7
Adnan Ashif <adnan@binmile.com>
2023-05-09 19:58:41 +0530

    testing

---
+++ UbFactory/src/main/java/com/example/ubfactory/UbFactoryApplication.java
@@ -0,0 +1,14 @@
+package com.example.ubfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbFactoryApplication.class, args);
+		System.out.println("testing");
+	}
+
+}
