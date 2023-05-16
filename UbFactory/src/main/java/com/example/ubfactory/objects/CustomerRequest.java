ac85b8f8f8a4a38cbc056f37cfe8a7e23343c5cf
Amit Gupta <amit.gupta@binmile.com>
2023-05-16 20:35:02 +0530

    Worked on generic request and response

--- UbFactory/src/main/java/com/example/ubfactory/objects/CustomerRequest.java
+++ UbFactory/src/main/java/com/example/ubfactory/objects/CustomerRequest.java
@@ -1,8 +1,12 @@
 package com.example.ubfactory.objects;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
+@Getter
+@Setter
 public class CustomerRequest {
     protected Integer id;
     private String firstName;
