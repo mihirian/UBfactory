f428ff2f93102bbfacaf20de709c2554c628837f
Adnan Ashif <adnan@binmile.com>
2023-05-24 19:11:36 +0530

    sheeping

--- UbFactory/src/main/java/com/example/ubfactory/entities/Shipping.java
+++ UbFactory/src/main/java/com/example/ubfactory/entities/Shipping.java
@@ -1,12 +1,14 @@
 package com.example.ubfactory.entities;
 
+import lombok.Data;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.math.BigDecimal;
 import java.util.Set;
-
+@Data
 @Entity
 @Table(name = "shipping")
 public class Shipping extends BaseEntity {
